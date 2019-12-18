3ead216a81e2228573d30c7b21733411994e9e94
kniteDeveloper <knite51@gmail.com>
2019-12-18 16:04:53 +0100

    add view for the user details and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UsersComponent } from './users/users.component';
+import { UserViewComponent } from './users/user-view/user-view.component';
 
 const routes: Routes = [
   { path: 'adminDashboard', component: DashboardComponent },
-  { path: 'usersInsight', component: UsersComponent, children: [
-    // {
-    //   path: '/:userId', component: ApplyComponent
-    // },
-  ]},
+  { path: 'usersInsight', component: UsersComponent},
+  {
+    path: 'usersInsight/:userId', component: UserViewComponent
+  },
   { path: '', redirectTo: 'adminDashboard', pathMatch: 'full' },
   { path: '**', redirectTo: 'adminDashboard', pathMatch: 'full' },
 ];
