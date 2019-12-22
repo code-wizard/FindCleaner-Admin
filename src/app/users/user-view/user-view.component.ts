127cac4e103675925b3dc68ac610c14225d610d0
Ayotunde Olubiyo <knite51@gmail.com>
2019-12-22 07:45:54 +0100

    ts refactor and add filter function, settings nav

--- src/app/users/user-view/user-view.component.ts
+++ src/app/users/user-view/user-view.component.ts
@@ -1,26 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-import { GeneralService } from 'src/app/services/general.service';
+import { Component, OnInit } from "@angular/core";
+import { GeneralService } from "src/app/services/general.service";
+import { EndpointsService } from "src/app/services/config/endpoints.service";
+import { ActivatedRoute, Params } from "@angular/router";
+import Swal from "sweetalert2";
 
 @Component({
-  selector: 'app-user-view',
-  templateUrl: './user-view.component.html',
-  styleUrls: ['./user-view.component.css']
+  selector: "app-user-view",
+  templateUrl: "./user-view.component.html",
+  styleUrls: ["./user-view.component.css"]
 })
 export class UserViewComponent implements OnInit {
+  userDetails = {};
+  constructor(
+    private genServ: GeneralService,
+    private endpoints: EndpointsService,
+    private router: ActivatedRoute
+  ) {
+    this.router.params.subscribe((res: Params) => {
+      const { username } = res;
+      this.endpoints.fetchOneUser(username).subscribe(res => {
+        this.userDetails = res;
+      });
+    });
+  }
 
-  constructor(private genServ: GeneralService) { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  private get updatedUserDetails(): any {
+    let validationFields = "";
+    const obj = this.userDetails;
+
+    for (const key in obj) {
+      if (!obj[key]) {
+        validationFields += `${key} cannot be blank <br/>`;
+      }
+    }
+    return !validationFields ? { ...this.userDetails } : validationFields;
   }
 
   handleUpdate() {
-    this.genServ.sweetAlertUpdates('Users');
+    const updatedUserDetails = this.updatedUserDetails;
+    if (typeof updatedUserDetails === "string") {
+      this.genServ.sweetAlertHTML("Validation", updatedUserDetails);
+    } else {
+      this.genServ.sweetAlertUpdates("Users").then(response => {
+        if (response.value) {
+          this.endpoints
+            .updateUser(updatedUserDetails.username, updatedUserDetails)
+            .subscribe(
+              res => {
+                console.log(res);
+                this.genServ.sweetAlertSucess(
+                  "User Update",
+                  "Update Successful"
+                );
+              },
+              error => {
+                console.log(error, "error on update");
+                this.genServ.sweetAlertError("Sorry, Update Not Successful");
+              }
+            );
+        }
+      });
+    }
   }
 
   handleDelete() {
-    this.genServ.sweetAlertDeletions('User');
+    this.genServ.sweetAlertDeletions("User");
   }
-
-
-
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,37 +1,43 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
-import { EndpointsService } from '../services/config/endpoints.service';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { MatPaginator, MatSort, MatTableDataSource } from "@angular/material";
+import { EndpointsService } from "../services/config/endpoints.service";
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  selector: "app-users",
+  templateUrl: "./users.component.html",
+  styleUrls: ["./users.component.css"]
 })
 export class UsersComponent implements OnInit {
-  myplaceHolder = 'Filter';
+  myplaceHolder = "Filter";
+  totalItemCount = 0;
+  paginationUrl = {
+    next: "",
+    previous: ""
+  };
   displayedColumns: string[] = [
-    'index',
-    'firstName',
-    'lastName',
-    'accountType',
-    'email',
-    'phoneNumber',
-    'action'
+    "index",
+    "firstName",
+    "lastName",
+    "accountType",
+    "email",
+    "phoneNumber",
+    "action"
   ];
   dataSource: MatTableDataSource<any>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private endpoints: EndpointsService) {
-  }
+  constructor(private endpoints: EndpointsService) {}
 
   ngOnInit() {
     this.endpoints.fetchAllUsers().subscribe((res: any) => {
-      console.log(res, 'reuls');
+      console.log(res, "reuls");
       const { results, count, next, previous } = res;
+      this.totalItemCount = count;
+      this.paginationUrl = { next, previous };
       this.dataSource = new MatTableDataSource(results);
-      this.dataSource.paginator = this.paginator;
+      // this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
     });
   }
@@ -43,17 +49,18 @@ export class UsersComponent implements OnInit {
     }
   }
 
-  checkPlaceHolder(value) {
-    console.log(value, 'vkfj')
-    if (value) {
-      this.myplaceHolder = null;
+  checkPlaceHolder(value, type) {
+    if (type === "onFocus" || value) {
+      this.myplaceHolder = "";
       return;
-    } else {
-      this.myplaceHolder = 'Filter';
+    } else if (type === "onBlur" && !value) {
+      this.myplaceHolder = "Filter";
       return;
     }
   }
 
-
-
+  handlePagination(event) {
+    console.log("working", event, this.paginationUrl);
+    this.endpoints.fetchPaginationPage;
+  }
 }
